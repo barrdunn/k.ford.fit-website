a15f3225804b32c4d6b17dda05d3ad214ffd7039
barrdunn <barrdunn@me.com>
2020-07-16 21:09:13 -0500

    finished desktop and mobile layouts

--- resources/js/script.js
+++ resources/js/script.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
         $(".js--teal-header-disappear--private-training").css({"color":"","transition":"all 0.4s","background-color":""});
         $(".js--white-header__private-training--appear").css({"color":"","background-color":"","transition":"all 0.4s"});
         $(".js--white-paragraph__private-training--appear").css({"color":"","transition":"all 0.4s"});
-        $(".js--service-box__text-container--private-training--blur").css({"backdrop-filter":"","-webkit-backdrop-filter":"blur(8px)","transition":"all 0.4s"});
+        $(".js--service-box__text-container--private-training--blur").css({"backdrop-filter":"","-webkit-backdrop-filter":"","transition":"all 0.4s"});
       });   
       
       $(".service-box__bg-image--nutrition-coach").hover(function() {
@@ -40,5 +40,26 @@ $(document).ready(function () {
         $(".js--service-box__text-container--dna-based--blur").css({"backdrop-filter":"","-webkit-backdrop-filter":"","transition":"all 0.4s"});
 
       });    
+
+
+      $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon ion-icon')
+
+
+        nav.slideToggle(200); 
+
+        if (icon.prop('name') == 'menu-outline') {
+          icon.removeProp('name', 'menu-outline');
+          icon.prop('name', 'close-outline');
+          icon.css({'position':'relative','top':'4rem'});
+      } else {
+          icon.removeProp('name', 'close-outline');
+          icon.prop('name', 'menu-outline');
+          icon.css({'position':'','top':''})
+      }
+
+
+      });
 
  });
